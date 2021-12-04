536c61cfefe03f635e253d822a9564ddeb848746
Orest Andrusyshyn <orest.andrusyshyn@onapp.com>
2021-12-05 00:48:09 +0200

    againx3 final warnings

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -6,7 +6,7 @@ public final class TempSummaryStatistics {
     private final double minTemp;
     private final double maxTemp;
     public TempSummaryStatistics(double avgTemp, double devTemp,
-                                 double minTemp, double maxTemp){
+                                 double minTemp, double maxTemp) {
         this.avgTemp = avgTemp;
         this.devTemp = devTemp;
         this.minTemp = minTemp;
